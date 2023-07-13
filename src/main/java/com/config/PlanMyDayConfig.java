d007192d8653e4ea8f30919b8885826e828c92b3
Alvin Xu <alvin3xu1@gmail.com>
2023-07-12 23:18:22 -0700

    set up project with only java

---
+++ src/main/java/com/config/PlanMyDayConfig.java
@@ -0,0 +1,20 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages="com.controllers")
+public class PlanMyDayConfig {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/views/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
+}
---
+++ src/main/java/com/config/PlanMyDayInitializer.java
@@ -0,0 +1,26 @@
+package com.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class PlanMyDayInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// System.out.println("Servlet settings");
+		
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(PlanMyDayConfig.class);
+		
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+		
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+	}
+
+}
