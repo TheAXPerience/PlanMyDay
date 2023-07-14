5b7ffa3da3bd0c9b7c0337caceb99cd97449f603
Alvin Xu <alvin3xu1@gmail.com>
2023-07-14 14:40:50 -0700

    add thymeleaf

--- src/main/java/com/config/PlanMyDayConfig.java
+++ src/main/java/com/config/PlanMyDayConfig.java
@@ -5,16 +5,51 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages="com.controllers")
 public class PlanMyDayConfig {
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		
+		templateResolver.setCacheable(false);
+		return templateResolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver viewResolver() {
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] {".html", ".xhtml"});
+		return viewResolver;
+	}
+	
+	/* basic
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver vr = new InternalResourceViewResolver();
 		vr.setPrefix("/WEB-INF/views/");
 		vr.setSuffix(".jsp");
 		return vr;
-	}
+	} */
 }
